c543b23bcc7b041d009d082a3bf1b74d97052a9b
rishabh-tripathi1 <tripathirishi80@gmail.com>
2024-01-18 22:23:00 +0530

    Vulnerability Added

--- client/src/Components/Pages/Home.jsx
+++ client/src/Components/Pages/Home.jsx
@@ -4,7 +4,6 @@ import Navbar from '../Elements/Navbar';
 import Desasters from './Disasters.jsx';
 import ContactUs from './ContactUs.jsx';
 import Blog_page from './Search.jsx';
-import Blog_page from './Blog_page.jsx';
 import SubscribeForm from '../Elements/Subscribeform';
 
 
--- client/src/Components/Pages/index.js
+++ client/src/Components/Pages/index.js
@@ -3,6 +3,7 @@ import ContactUs from './ContactUs';
 import Volunteer from './Volunteer';
 import Disasters from './Disasters';
 import Search from './Search';
+import Image from './Image'
 
 
-export { Home, ContactUs, Volunteer, Disasters, Search }
+export { Home, ContactUs, Volunteer, Disasters, Search, Image }

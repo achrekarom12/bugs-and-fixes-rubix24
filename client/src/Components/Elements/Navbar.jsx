0178ec54a2674c699f0a288064c5333876cfd744
rishabh-tripathi1 <tripathirishi80@gmail.com>
2024-01-18 18:53:51 +0530

    Router Nodes

--- client/src/Components/Elements/Navbar.jsx
+++ client/src/Components/Elements/Navbar.jsx
@@ -4,8 +4,8 @@ const Navbar = (props) => {
   return (
     <div className='w-full h-[78px] bg-transparent pl-[18px] py-[20px]'>
       <div className='grid grid-flow-col'>
-        <img src='./icons/menue.svg' onClick={()=> {props.visiblitieupdate(0)}} className={`w-[45px] h-[38px] hover:scale-150 transition-transform duration-300 ease-in-out`}/>
-        <img src='./icons/x.png' onClick= {()=>{props.visiblitieupdate(1)}} className='w-[40px] h-[40px] hover:scale-125 transition-transform duration-300 ease-in-out'/>
+        <img src='./icons/menue.svg' onClick={() => { props.visiblitieupdate(0) }} className={`w-[45px] h-[38px] hover:scale-150 transition-transform duration-300 ease-in-out`} />
+        <img src='./icons/x.png' onClick={() => { props.visiblitieupdate(1) }} className='w-[40px] h-[40px] hover:scale-125 transition-transform duration-300 ease-in-out' />
         {/* <img className='text-[#ffff]' src='./logo/Logo.png'/> */}
       </div>
     </div>
--- client/src/Components/Pages/index.js
+++ client/src/Components/Pages/index.js
@@ -1,9 +1,7 @@
 import Home from './Home';
 import ContactUs from './ContactUs';
-import Apply_Volentier from './Apply_Volentier';
-import Login from "./Login";
-import Evacuation_routes from './Evacuation_routes';
-import Signup from './Signup';
+import Volunteer from './Volunteer';
+import Desasters from './Desasters';
 
 
-export{Home, ContactUs, Apply_Volentier, Login, Evacuation_routes, Signup}
+export { Home, ContactUs, Volunteer, Desasters }

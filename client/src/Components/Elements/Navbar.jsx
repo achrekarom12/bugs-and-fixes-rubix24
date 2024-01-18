70d3c22820d8e127063dfaa9893f61a73fa580e2
pravinshinde99300@gmail.com <pravinshinde99300@gmail.com>
2024-01-18 22:33:23 +0530

    search bar improved

--- client/src/Components/Elements/Navbar.jsx
+++ client/src/Components/Elements/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from 'D:\\Rubix\\bugs-and-fixes-rubix24\\client\\src\\assets\\Logo.png';
+import logo from '../../assets/Logo.png';
 
 const Navbar = (props) => {
   return (
--- client/src/Components/Pages/Search.jsx
+++ client/src/Components/Pages/Search.jsx
@@ -12,9 +12,9 @@ const Search = () => {
   }
   return (
     <div className='h-screen w-full'>
-      <div className='flex flex-col justify-center items-center w-full'>
+      <div className='flex flex-col justify-center items-center w-full '>
         <div className='flex flex-row gap-3'>
-          <input className='h-[30px] w-[600px]' placeholder='Search' />
+          <input className='h-[30px] w-[600px] input-border border-none focus:outline-none' placeholder='Search'/>
           <button className='w-auto h-auto bg-red-500'>Search</button>
         </div>
         <div className='w-full h-screen bg-white mt-2 rounded-lg flex flex-col gap-10 justify-center items-center'>
